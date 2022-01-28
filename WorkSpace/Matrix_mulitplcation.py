e04260f66f83f76da7f38bbb7802e0e168b041df
callumthomas086 <45464268+callumthomas086@users.noreply.github.com>
2022-01-28 09:48:46 +0000

    CSV_4_to_150

--- WorkSpace/Matrix_mulitplcation.py
+++ WorkSpace/Matrix_mulitplcation.py
@@ -9,8 +9,8 @@ import time
 
 
 
-matrix_a = np.random.rand(1000,1000)
-matrix_b = np.random.rand(1000,1000)
+matrix_a = np.random.rand(150,150)
+matrix_b = np.random.rand(150,150)
 
 
 
@@ -39,7 +39,7 @@ matrix_A_inverse= inverse_matrix()
 proof_of_inverse = matrix_A_inverse.dot(matrix_a)
 
 write_to_txt = pd.DataFrame(proof_of_inverse)
-#write_to_txt.to_csv('inverse_matrix_1000.csv')
+write_to_txt.to_csv('inverse_matrix_150.csv')
 
 
 
@@ -53,14 +53,6 @@ write_to_txt = pd.DataFrame(proof_of_inverse)
 
 
 
-print ("New matrix A:")
-print (matrix_a)
-print ("\n")
-print ("New matrix B:")
-print (matrix_b)
-print ("\n")
-print ("Matrix A times matrix B:")
-print (A_time_B)
 
 
 
@@ -70,16 +62,4 @@ print (A_time_B)
 
 
 
-
-
-
-print ('\n')
-# print ('\n')
-# print (product)
-print ("Matrix A inverse:")
-print (matrix_A_inverse)
-print ('\n')
-
-print ('\n')
-print (proof_of_inverse)
 
