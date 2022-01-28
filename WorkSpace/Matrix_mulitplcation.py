219960497c8b781f0b45187e73389285c31a7c77
callumthomas086 <callumthomas086@gmail.com>
2022-01-28 13:06:00 +0000

    complete model

--- WorkSpace/Matrix_mulitplcation.py
+++ WorkSpace/Matrix_mulitplcation.py
@@ -1,51 +1,58 @@
 import numpy as np
 import pandas as pd
+from datetime import datetime
 import time 
 
 
 
-
-
-
-
-
-matrix_a = np.random.rand(150,150)
-matrix_b = np.random.rand(150,150)
-
-
-
+matrix_a = np.random.randint(1, 10, size=(2048, 2048))
+# matrix_b = np.random.rand(200,200)
 
 # create a function to multiply n dimensions matrix
-
-def matrix_multiplction_n_times(matrix_a, matrix_b):
-    A_time_B = np.dot(matrix_a, matrix_b)
-    return A_time_B
-
-
-A_time_B = matrix_multiplction_n_times(matrix_a, matrix_b)
-
-time_end = time.time()
+# def matrix_multiplction_n_times(matrix_a, matrix_b):
+#     A_time_B = np.dot(matrix_a, matrix_b)
+#     return A_time_B
 
 
+multiplcation_start_time = datetime.now()
 
+# muliply matrix_a by itself n times
+def multiply_matrix_n_times(matrix_a, n):
+    for i in range(n):
+        matrix_a = np.dot(matrix_a, matrix_a)
+        
+    return matrix_a
+
+run_multiplcation_10_times = multiply_matrix_n_times(matrix_a, 10)
+
+
+# A_time_B = matrix_multiplction_n_times(matrix_a, matrix_b)
+multiplcation_end_time = datetime.now()
+
+inversion_start_time = datetime.now()
 def inverse_matrix():
     matrix_A_inverse = np.linalg.inv(matrix_a)
 
     return matrix_A_inverse
 
 
+
 matrix_A_inverse= inverse_matrix()
-
 proof_of_inverse = matrix_A_inverse.dot(matrix_a)
+inversion_end_time = datetime.now()
+# these are commented out as they were used as proof of concept
 
-write_to_txt = pd.DataFrame(proof_of_inverse)
-write_to_txt.to_csv('inverse_matrix_150.csv')
+# wirtie_proof_of_inverse = pd.DataFrame(proof_of_inverse)
+# wirtie_proof_of_inverse.to_csv('proof_of_inverse.csv')
+# # write_original_matrix_to_txt = pd.DataFrame(matrix_a)
+# # write_original_matrix_to_txt.to_csv('Matrix_a_origonal_int.csv')
+# # write_multiplcation_to_txt = pd.DataFrame(run_multiplcation_10_times)
+# # write_multiplcation_to_txt.to_csv('Matrix_a_multiplcation_10_times_int.csv')
 
 
+print ("Duration of multiplcation: {}".format(multiplcation_end_time - multiplcation_start_time))
+print ("Duration of inversion: {}".format(inversion_end_time - inversion_start_time))
 
-
-
-# All the prints
 
 
 
