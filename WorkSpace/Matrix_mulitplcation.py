053984a6e8c039bc39ee78bd111a6c757678d2d5
callumthomas086 <callumthomas086@gmail.com>
2022-02-02 10:04:00 +0000

    clean code

--- WorkSpace/Matrix_mulitplcation.py
+++ WorkSpace/Matrix_mulitplcation.py
@@ -1,65 +1,60 @@
+import time
+import numpy
+
+
 import numpy as np
+import random 
 import pandas as pd
 from datetime import datetime
-import time 
+import time
+
+# print( "%.16f" % float("1.953125e-02"))
+# DON'T TOUCH THIS CODE
+matrix = np.random.rand(1000, 1000)
+matrix_b = matrix
 
 
-
-matrix_a = np.random.randint(1, 10, size=(50, 50))
-# matrix_b = np.random.rand(200,200)
-
-# create a function to multiply n dimensions matrix
-# def matrix_multiplction_n_times(matrix_a, matrix_b):
-#     A_time_B = np.dot(matrix_a, matrix_b)
-#     return A_time_B
-
-
+# Start the Timer for the multiplication
 multiplcation_start_time = datetime.now()
 
-# muliply matrix_a by itself n times
-def multiply_matrix_n_times(matrix_a, n):
+def matrix_multi(matrix, matrix_b, n):
     for i in range(n):
-        matrix_a = np.dot(matrix_a, matrix_a)
-        
-    return matrix_a
-
-run_multiplcation_10_times = multiply_matrix_n_times(matrix_a, 10)
+        matrix = np.dot(matrix, matrix_b)
+    return matrix
 
 
-# A_time_B = matrix_multiplction_n_times(matrix_a, matrix_b)
+# Start the Timer for the multiplication
+multiplcation_start_time = datetime.now()
+time.sleep(1)
+# Rn the multiplication 10 times
+Muliplied_10_times = matrix_multi(matrix, matrix_b, 10)
+
+# End the Timer for the multiplication
 multiplcation_end_time = datetime.now()
 
-inversion_start_time = datetime.now()
-def inverse_matrix():
-    matrix_A_inverse = np.linalg.inv(matrix_a)
-
-    return matrix_A_inverse
-
-
-
-matrix_A_inverse= inverse_matrix()
-proof_of_inverse = matrix_A_inverse.dot(matrix_a)
-inversion_end_time = datetime.now()
-# these are commented out as they were used as proof of concept
-
-# wirtie_proof_of_inverse = pd.DataFrame(proof_of_inverse)
-# wirtie_proof_of_inverse.to_csv('proof_of_inverse.csv')
-# # write_original_matrix_to_txt = pd.DataFrame(matrix_a)
-# # write_original_matrix_to_txt.to_csv('Matrix_a_origonal_int.csv')
-# # write_multiplcation_to_txt = pd.DataFrame(run_multiplcation_10_times)
-# # write_multiplcation_to_txt.to_csv('Matrix_a_multiplcation_10_times_int.csv')
-
-
 print ("Duration of multiplcation: {}".format(multiplcation_end_time - multiplcation_start_time))
-print ("Duration of inversion: {}".format(inversion_end_time - inversion_start_time))
+print ("\n")
+
+# Start the Timer for the inversion
+Inersion_start_time = datetime.now()
+
+inverse = np.linalg.inv(matrix)
+
+# End the Timer for the inversion
+Inversion_end_time = datetime.now()
+
+print ("Duration of inversion: {}".format(Inversion_end_time - Inersion_start_time))
 
 
 
+def Proof_of_inv(matrix, inverse):
+    proof_of_inverse = np.dot(inverse, matrix)
+    return proof_of_inverse
 
-
-
-
-
+'''Uncommect this to write the proof of inverse to a csv file
+proof_of_inverse = Proof_of_inv(matrix, inverse)
+wirtie_proof_of_inverse = pd.DataFrame(proof_of_inverse)
+wirtie_proof_of_inverse.to_csv('proof_of_inverse.csv')'''
 
 
 
