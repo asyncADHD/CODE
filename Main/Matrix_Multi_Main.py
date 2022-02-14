4c3eb69bb9dab799c07d243f63784dedc26e4724
callumthomas086 <callumthomas086@gmail.com>
2022-02-14 12:33:17 +0000

    Clean up

--- Main/Matrix_Multi_Main.py
+++ Main/Matrix_Multi_Main.py
@@ -1,9 +1,34 @@
 import numpy as np
 import pandas as pd 
+from datetime import datetime
 
 
 
-def matrix_multiplcation(Matrix, Matrix_A, n):
+s = datetime.now()
+
+matrix = np.random.rand(50, 50)
+matrix_a = matrix
+
+
+
+def matrix_multi(matrix, matrix_b, n):
     for i in range(n):
-        Matrix_A = np.dot(Matrix_A, Matrix_B)
-    return Matrix_A
+        matrix = np.dot(matrix, matrix_b)
+        
+    return matrix
+
+
+
+Muliplied_10_times = matrix_multi(matrix, matrix_a, 11)
+write_multi_to_csv = pd.DataFrame(matrix)
+write_multi_to_csv.to_csv('matrix_multi_50.csv')
+
+
+
+e = datetime.now()
+
+print (e-s)
+
+
+
+
