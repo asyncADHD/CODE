23e6621a2731d86563ba97a4f455a4eb675e0e93
callumthomas086 <callumthomas086@gmail.com>
2022-03-10 09:17:57 +0000

    still not working

--- ideaTesting/arrayQuery.py
+++ ideaTesting/arrayQuery.py
@@ -1,17 +1,23 @@
+from re import I
 import pandas as pd
 import numpy as np
 import math
 
 
-arr = np.array([[1,1,1],[2,140,2],[3,3,3]])
+price_list = [[98, 104, 138, 141, 185], [2,123,142,123,124]]
 
 
-def arrayQuery(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr[i])):
-            if arr[i][j] == 140:
-                return i,j
-    return None
 
-qur_arr = arrayQuery(arr)
-print(qur_arr)
+a = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        print(a[i][j], end=' ')
+    print()
+        
+        
+        
+
+print (main(price_list))
+
+
+
