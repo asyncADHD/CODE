ef80408a842a83184ff42dd8c29fa99186e066d0
callumthomas086 <callumthomas086@gmail.com>
2022-03-28 08:56:45 +0100

    no change

--- ideaTesting/tst1.py
+++ ideaTesting/tst1.py
@@ -1,3 +1,4 @@
+
 import numpy as np 
 import math
 import pandas as pd
@@ -9,9 +10,24 @@ arr = [[1,2,3],[3,3,3],[1,1,1]]
 
 x = 3
 
-for i in range (0,len(arr)):
+
+
+for i in range(len(arr)):
     if x in arr[i]:
         # print the psoition of x in the array
-        
+        print("dummy")
+
         
 
+
+def enumerate_list(arr):
+    for i, x in enumerate(arr):
+        print(i, x)
+    
+def enumrate_dict(arr):
+    x = 2
+    for i in range(len(arr)):
+        if x in arr[i]:
+            print (arr.index(x))
+        else:
+            print("dummy")
